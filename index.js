c064bf5f35f42785541f7dd66b5bcde81ed9e2f8
Antonio <jconradolopez19@gmail.com>
2024-02-19 10:20:29 -0600

    se agregó la ruta para /veterinarios y express.json()

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ import express from 'express';
 import dotenv  from 'dotenv';
 import conectarDB from './config/db.js';
 
+//import routes
+import veterinarioRoutes from  './routes/veterinarioRoutes.js';
+
 
 const app = express();
+app.use(express.json());//leer datos JSON enviados en una solicitud HTTP.
 
 dotenv.config();
 conectarDB();
 
+
+//url de routes
+app.use('/veterinarios', veterinarioRoutes); 
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
