a2806f5070481e9a38cc1f181d9cc61099369942
Antonio <jconradolopez19@gmail.com>
2024-02-24 07:34:45 -0600

    Habilitando cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv  from 'dotenv';
+import cors from 'cors';
 import conectarDB from './config/db.js';
 
 //import routes
@@ -13,7 +14,19 @@ app.use(express.json());//leer datos JSON enviados en una solicitud HTTP.
 dotenv.config();
 conectarDB();
 
+const dominiosPermitidos = ['http://localhost:5173'];
+const corsOptions = {
+    origin : function(origin, callback){
+        if(dominiosPermitidos.indexOf(origin) !== -1){
+            //el origen del request esta permitido
+            callback(null, true);
+        }else{
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+};
 
+app.use(cors(corsOptions));
 //url de routes
 app.use('/veterinarios', veterinarioRoutes); 
 app.use('/pacientes', pacienteRoutes);
