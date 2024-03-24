b7d7bbb70017c6e4e49285acd6f968952aaa4fea
Antonio <jconradolopez19@gmail.com>
2024-03-24 16:28:50 -0600

    Enviar email para confirmar cuenta o restablecer password  al usuario

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -2,6 +2,7 @@ import nodemailer  from 'nodemailer';
 
 const emailNuevoPassword = async (datos) =>{
     var transport = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
         secure: false,
@@ -22,14 +23,11 @@ const emailNuevoPassword = async (datos) =>{
         to : email,
         subject : 'Restablece tu password',
         text : 'Restablece tu password',
-        html : `<p> Hola : ${nombre} has solicítado restablecer tu password.</p>
-                <p> Sigue el siguiente enlace para generar un nuevo password.</p>
-                <a href='${process.env.FRONTEND_URL}/reset-password/${token}'> Restablecer password</a>
-
-                <p>Si tú no has solicítado restablecer tu password, puedes ignora este mensaje!</p>
-        
-        `
-    });
+        html : `<p style=" color: rgb(75 85 99); text-align: center; font-size: 16px;"> Hola : ${nombre} has solicítado restablecer tu password.</p>
+                <p  style=" color: rgb(75 85 99);"> Sigue el siguiente enlace para generar un nuevo password.</p>
+                <a href='${process.env.FRONTEND_URL}/reset-password/${token}' style="background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: block; font-size: 16px; margin: 4px 2px; margin:0 auto; cursor: pointer; border-radius: 8px;"> Restablecer password</a>
+                <p style="color: red;">Si tú no has solícitado restablecer el password, puedes ignorar este mensaje.</p>   
+        `});
 
     console.log('mensaje enviado : %s', info.messageId);
 };
--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -2,13 +2,13 @@ import nodemailer  from 'nodemailer';
 
 const emailRegistro = async (datos) =>{
     var transport = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
         secure: false,
         auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
-        
         },
         tls: {
             ciphers: "TLSv1.2,TLSv1.3",
@@ -22,14 +22,11 @@ const emailRegistro = async (datos) =>{
         to : email,
         subject : 'Confirmar cuenta',
         text : 'Confirmar cuenta en Administrador de pacientes de Veterinaria',
-        html : `<p> Hola : ${nombre} confirma tu cuenta.</p>
-                <p> Tu cuenta ya está lista, solo debes confirmarla en el siguiente enlace.</p>
-                <a href='${process.env.FRONTEND_URL}/confirmar-cuenta/${token}'> Comfirmar Cuenta</a>
-
-                <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje!</p>
-        
-        `
-    });
+        html: `<p style=" color: rgb(75 85 99); text-align: center; font-size: 16px;">Hola ${nombre}, Por favor confirma tu cuenta!</p>
+            <p style=" color: rgb(75 85 99);">Tu cuenta ya está lista, solo debes confirmarla en el siguiente enlace:</p>
+            <a href='${process.env.FRONTEND_URL}/confirmar-cuenta/${token}' style="background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: block; font-size: 16px; margin: 4px 2px; margin:0 auto; cursor: pointer; border-radius: 8px;">Confirmar Cuenta</a>
+            <p style="color: red;">Si tú no creaste esta cuenta, puedes ignorar este mensaje.</p>`
+}); 
 
     console.log('mensaje enviado : %s', info.messageId);
 };
